4f7b57a96e34c7fa5b1ea98b55c5dfc412f67af6
Kristen Picard <kristenpicard93@gmail.com>
2021-04-12 21:06:10 -0400

    Setting up seeds file

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+USE cms_DB;
+
+INSERT INTO employee (first_name, last_name, role_id)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+INSERT INTO role (title, salary, department_id)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+INSERT INTO department (name)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+
+
+
+
+
+
