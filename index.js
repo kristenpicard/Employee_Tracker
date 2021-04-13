5a065f69c8c2599d5202a3d5c0ae476640ab49d2
Kristen Picard <kristenpicard93@gmail.com>
2021-04-12 20:19:44 -0400

    adding code to connect sql to js file

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "cms_DB",
+});
+
 inquirer
   .prompt([
     {
---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS cms_DB;
+CREATE DATABASE cms_DB;
+
+USE cms_DB;
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT default 0 NOT NULL,
+  manager_id INT default 0,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT default 0 NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
