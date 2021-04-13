0bfead993c97c45710b4f1f8c8b373ad7bd086e8
Kristen Picard <kristenpicard93@gmail.com>
2021-04-12 21:16:33 -0400

    added to seeds

--- seeds.sql
+++ seeds.sql
@@ -4,10 +4,13 @@ INSERT INTO employee (first_name, last_name, role_id)
 -- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
 
 INSERT INTO role (title, salary, department_id)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), 
+("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
 
 INSERT INTO department (name)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
 
 
