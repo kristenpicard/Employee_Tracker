2c4d7a08fe6e38e08e02edd065cb7539c9dbac44
Kristen Picard <kristenpicard93@gmail.com>
2021-04-12 20:06:41 -0400

    Adding .sql schema page with tables

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+inquirer
+  .prompt([
+    {
+      message: "Let's Build Your Team Profile! (Press Enter to begin)",
+      name: "mssg",
+    },
+    {
+      type: "input",
+      message: "Enter the Manager's name: ",
+      name: "name",
+      validate: function (input) {
+        if (input == " ") {
+          console.log("Please enter a valid name");
+          return false;
+        }
+        return true;
+      },
+    },
+  ])
+  .then((data) => {
+    const name = data.name;
+    const email = data.email;
+    const id = data.id;
+    const onumber = data.onumber;
+
+    const member = new Manager(name, email, id, onumber);
+    myTeam.push(member);
+    addTeammate();
+  });
