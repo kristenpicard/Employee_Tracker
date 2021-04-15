018d42140312a56605f5a5f5941b52a89ab86324
Kristen Picard <kristenpicard93@gmail.com>
2021-04-15 11:43:49 -0400

    Working on View All Function

--- server.js
+++ server.js
@@ -60,149 +60,139 @@ const start = () => {
 // Function to view all employees
 const viewAll = () => {
   // FIGURE OUT HOW TO SHOW EMPLOYEE DATA
-  connection.query(
-    "INSERT INTO auctions SET ?",
-    // QUESTION: What does the || 0 do?
-    {
-      item_name: answer.item,
-      category: answer.category,
-      starting_bid: answer.startingBid || 0,
-      highest_bid: answer.startingBid || 0,
-    },
-    (err) => {
-      if (err) throw err;
-      console.log("Your auction was created successfully!");
-      // re-prompt the user for if they want to bid or post
-      start();
-    }
-  );
+  connection.query("SELECT * FROM employee", (err) => {
+    if (err) throw err;
+    console.log("Success!");
+    // re-prompt the user for what they want to do next
+    // start();
+  });
 };
 
 // Function to add a new employee
-const addEmp = () => {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM auctions", (err, results) => {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices() {
-            const choiceArray = [];
-            results.forEach(({ item_name }) => {
-              choiceArray.push(item_name);
-            });
-            return choiceArray;
-          },
-          message: "What auction would you like to place a bid in?",
-        },
-        {
-          name: "bid",
-          type: "input",
-          message: "How much would you like to bid?",
-        },
-      ])
-      .then((answer) => {
-        // get the information of the chosen item
-        let chosenItem;
-        results.forEach((item) => {
-          if (item.item_name === answer.choice) {
-            chosenItem = item;
-          }
-        });
+// const addEmp = () => {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM auctions", (err, results) => {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     inquirer
+//       .prompt([
+//         {
+//           name: "choice",
+//           type: "rawlist",
+//           choices() {
+//             const choiceArray = [];
+//             results.forEach(({ item_name }) => {
+//               choiceArray.push(item_name);
+//             });
+//             return choiceArray;
+//           },
+//           message: "What auction would you like to place a bid in?",
+//         },
+//         {
+//           name: "bid",
+//           type: "input",
+//           message: "How much would you like to bid?",
+//         },
+//       ])
+//       .then((answer) => {
+//         // get the information of the chosen item
+//         let chosenItem;
+//         results.forEach((item) => {
+//           if (item.item_name === answer.choice) {
+//             chosenItem = item;
+//           }
+//         });
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                highest_bid: answer.bid,
-              },
-              {
-                id: chosenItem.id,
-              },
-            ],
-            (error) => {
-              if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
-            }
-          );
-        } else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
-        }
-      });
-  });
-};
+//         // determine if bid was high enough
+//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
+//           // bid was high enough, so update db, let the user know, and start over
+//           connection.query(
+//             "UPDATE auctions SET ? WHERE ?",
+//             [
+//               {
+//                 highest_bid: answer.bid,
+//               },
+//               {
+//                 id: chosenItem.id,
+//               },
+//             ],
+//             (error) => {
+//               if (error) throw err;
+//               console.log("Bid placed successfully!");
+//               start();
+//             }
+//           );
+//         } else {
+//           // bid wasn't high enough, so apologize and start over
+//           console.log("Your bid was too low. Try again...");
+//           start();
+//         }
+//       });
+//   });
+// };
 
-// Function to update an employee role
-const updEmpRole = () => {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM auctions", (err, results) => {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices() {
-            const choiceArray = [];
-            results.forEach(({ item_name }) => {
-              choiceArray.push(item_name);
-            });
-            return choiceArray;
-          },
-          message: "What auction would you like to place a bid in?",
-        },
-        {
-          name: "bid",
-          type: "input",
-          message: "How much would you like to bid?",
-        },
-      ])
-      .then((answer) => {
-        // get the information of the chosen item
-        let chosenItem;
-        results.forEach((item) => {
-          if (item.item_name === answer.choice) {
-            chosenItem = item;
-          }
-        });
+// // Function to update an employee role
+// const updEmpRole = () => {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM auctions", (err, results) => {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     inquirer
+//       .prompt([
+//         {
+//           name: "choice",
+//           type: "rawlist",
+//           choices() {
+//             const choiceArray = [];
+//             results.forEach(({ item_name }) => {
+//               choiceArray.push(item_name);
+//             });
+//             return choiceArray;
+//           },
+//           message: "What auction would you like to place a bid in?",
+//         },
+//         {
+//           name: "bid",
+//           type: "input",
+//           message: "How much would you like to bid?",
+//         },
+//       ])
+//       .then((answer) => {
+//         // get the information of the chosen item
+//         let chosenItem;
+//         results.forEach((item) => {
+//           if (item.item_name === answer.choice) {
+//             chosenItem = item;
+//           }
+//         });
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                highest_bid: answer.bid,
-              },
-              {
-                id: chosenItem.id,
-              },
-            ],
-            (error) => {
-              if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
-            }
-          );
-        } else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
-        }
-      });
-  });
-};
+//         // determine if bid was high enough
+//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
+//           // bid was high enough, so update db, let the user know, and start over
+//           connection.query(
+//             "UPDATE auctions SET ? WHERE ?",
+//             [
+//               {
+//                 highest_bid: answer.bid,
+//               },
+//               {
+//                 id: chosenItem.id,
+//               },
+//             ],
+//             (error) => {
+//               if (error) throw err;
+//               console.log("Bid placed successfully!");
+//               start();
+//             }
+//           );
+//         } else {
+//           // bid wasn't high enough, so apologize and start over
+//           console.log("Your bid was too low. Try again...");
+//           start();
+//         }
+//       });
+//   });
+// };
 
 // Connects to the mysql server and database
 connection.connect((err) => {
